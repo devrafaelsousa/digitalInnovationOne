f7ba3eddb42b29dd44c721b0d377473dabb632bb
devrafaelsousa <rafael.sousa.pereira@mail.usf.edu.br>
2021-01-23 01:34:32 -0300

    update

---
+++ parte02/collections/javaList/list/optionals/ExemploOptional.java
@@ -0,0 +1,28 @@
+package parte02.collections.javaList.list.optionals;
+
+import java.util.Optional;
+
+public class ExemploOptional {
+
+    public static void main(String[] args) {
+
+        Optional<String> optionalString = Optional.of("Valor opcional");
+
+        System.out.println(optionalString.isPresent());
+
+//        optionalString.isPresent(System.out::println);
+//
+//        optionalString.ifPresentOrElse(System.out::println,
+//                () -> System.out.println("Valor não está presente"));
+//        if (optionalString.isPresent()) {
+//            String valor = optionalString.get();
+//
+//            System.out.println(valor);
+//        }
+//
+//        optionalString.map((valor) -> valor.concat("***")).isPresent(System.out::printf);
+//
+//        optionalString.orElseThrow(IllegalAccessError::new);
+    }
+
+}
